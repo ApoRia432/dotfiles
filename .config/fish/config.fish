46ed4927677457aca7a31f73cd04e2a6b4bde191
ApoRia432 <lecterkn@proton.me>
2025-09-02 14:06:28 +0900

    [fish] update envs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,22 @@
 set fish_greeting
 
+# alisa for xbps
 alias xbps-install="sudo xbps-install"
 alias xbps-remove="sudo xbps-remove"
 
+# golang
+export GOPATH=$(go env GOPATH)
+export PATH="$PATH:$GOPATH/bin"
+
+# .local/bin
 export PATH="$HOME/.local/bin:$PATH"
 
 source "$HOME/.cargo/env.fish"
 
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# opencode
+fish_add_path /home/iuno/.opencode/bin
