b1e3f5dc14ae7a9d7d7521a471e2fde7610c7f00
yonagi <lecterkn@proton.me>
2025-09-14 05:36:53 +0900

    fix: disable logging

--- .config/niri/notify_brightness.sh
+++ .config/niri/notify_brightness.sh
@@ -7,5 +7,3 @@ percentage=$((current_brightness * 100 / max_brightness))
 
 notify-send -h string:x-canonical-private-synchronous:brightness_notification \
             -h int:value:"$percentage" "Brightness set to ${percentage}%"
-
-echo $percentage >> ~/brighness.log
