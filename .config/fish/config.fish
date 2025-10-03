220a6e31a846d5213accce1fafe284cec8ac612c
yonagi <lecterkn@proton.me>
2025-10-04 07:29:03 +0900

    refactor: fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,10 +15,6 @@ fish_add_path $HOME/.local/bin
 
 source "$HOME/.cargo/env.fish"
 
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
 # opencode
 fish_add_path /home/iuno/.opencode/bin
 
