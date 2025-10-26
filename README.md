1ed2273ca0700edbb85b61378de3f87378adc630
yonagi <lecterkn@proton.me>
2025-10-26 17:22:25 +0900

    feat: link.sh

--- README.md
+++ README.md
@@ -1,71 +1,7 @@
 # dotfiles
 
-## Requirements
+## how 2 use
 
-### Stow
-
-```sh
-brew install stow
-```
-
-## Installation
-
-### 1. Clone
-
-```sh
-git clone git@github.com:lecterkn/dotfiles.git ~/dotfiles
-```
-
-### 2. Run Stow
-
-```sh
-cd ~/dotfiles
-
-stow .
-```
-
-## Applications
-
-### TPM
-
-```sh
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-```
-
-### OhMyZsh
-
-```sh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-```
-
-### tmux
-
-```
-brew install tmux
-```
-
-### Sketchybar
-
-```sh
-brew install FelixKratz/formulae/sketchybar
-```
-
-### yabai
-
-```sh
-brew install koekeishiya/formulae/yabai
-```
-
-#### SIP
-
-Syetem Integrity Protection should be disabled
-
-```sh
-csrutil status
-```
-
-### skhd
-
-```sh
-brew install koekeishiya/formulae/skhd
+```bash
+./link.sh
 ```
---
+++ link.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+tmux="tmux/.tmux.conf"
+bash_rc="bash/.bashrc"
+bash_profile="bash/.bash_profile"
+
+ghostty="ghostty"
+nvim="nvim"
+aerospace="aerospace"
+skhd="skhd"
+
+link_file() {
+    link_file="$(pwd)/${1}"
+    file_name="$(basename ${1})"
+    link_target="${2}/$file_name"
+    if [[ -L "$link_target" ]]; then
+        echo -e "$file_name \033[32malready linked\033[0m"
+        return
+    fi
+    if [[ -f "$link_target" ]]; then
+        echo -e "$file_name \033[31mexists\033[0m"
+        return
+    fi
+    # create symbolic link
+    ln -s "$link_file" "$link_target" && echo -e "$file_name \033[32mlinked\033[0m"
+}
+
+link_file "$tmux" "$HOME"
+link_file "$bash_rc" "$HOME"
+link_file "$bash_profile" "$HOME"
+
+link_file "$ghostty" "$HOME/.config"
+link_file "$nvim" "$HOME/.config"
+link_file "$aerospace" "$HOME/.config"
+link_file "$skhd" "$HOME/.config"
