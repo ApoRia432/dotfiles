14b85c7eaa0dfeb7798eefcaf45cdf65965b24a7
ApoRia432 <lecterkn@proton.me>
2025-12-07 16:38:20 +0900

    theme: browser

--- browser_theme.sh
+++ browser_theme.sh
@@ -2,6 +2,7 @@
 
 theme_file="chromium/chromium.json"
 chrome_dir="/etc/opt/chrome/policies/managed"
+brave_dir="/etc/brave/policies/managed"
 chromium_dir="/etc/chromium/policies/managed"
 
 install_chromium() {
@@ -30,10 +31,30 @@ install_chrome() {
     cat "$theme_file" > "$chrome_dir/theme.json"
 }
 
+install_brave() {
+    if [[ ! -d $brave_dir ]]; then
+        sudo mkdir -p $brave_dir
+        sudo chmod a+rw $brave_dir
+    fi
+    if [[ -f "$brave_dir/theme.json" ]]; then
+        echo -e "brave-browser theme is \e[32mupdated\e[0m"
+    else
+        echo -e "\e[32msuccess\e[0m to installed brave-browser theme."
+    fi
+    cat "$theme_file" > "$brave_dir/theme.json"
+}
+
+update_browser() {
+    chromium --refresh-platform-policy --no-startup-window /dev/null 2>&1
+    google-chrome-stable --refresh-platform-policy --no-startup-window /dev/null 2>&1
+    brave-browser-stable --refresh-platform-policy --no-startup-window /dev/null 2>&1
+}
 
 if [[ -f "$theme_file" ]]; then
     install_chromium
     install_chrome
+    install_brave
+    update_browser
 else
     echo -e "\e[1m$theme_file\e[0m does \e[31m\e[1mnot\e[0m exists."
 fi
--- theme.sh
+++ theme.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
+update_browser() {
+    chromium --refresh-platform-policy --no-startup-window > /dev/null 2>&1
+    google-chrome-stable --refresh-platform-policy --no-startup-window > /dev/null 2>&1
+    brave-browser-stable --refresh-platform-policy --no-startup-window > /dev/null 2>&1
+}
+
 echo "Chose your theme (1: Dark, 2: Light)"
 echo -e "-> "
 read theme
 
 if [ $theme = "1" ]; then
-    gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+    gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-Dark'
     gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+    update_browser
     echo "theme set to 'Dark'"
 elif [ $theme = "2" ]; then
     gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita'
     gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
+    update_browser
     echo "theme set to 'Light'"
 else
     echo "invalid theme: $theme"
