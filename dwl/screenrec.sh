109f07bd3265034ced8cbcea98bda921d68887b1
ApoRia432 <lecterkn@proton.me>
2025-12-10 01:22:12 +0900

    dwl: screenrec script

---
+++ dwl/screenrec.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+## requirements
+# - slurp
+# $ xi slurp
+# - wl-screenrec
+# $ cargo install wl-screenrec
+#
+
+OUTPUT_DIR="$HOME/Videos"
+if [ ! -d "$OUTPUT_DIR" ]; then
+    mkdir -p "$OUTPUT_DIR"
+fi
+
+start_recording() {
+    local filename="$OUTPUT_DIR/ScreenRecord-$(date +'%Y-%m-%d_%H-%M-%S').mp4"
+
+    local grid="$(slurp)"
+    notify-send "Screen recording started."
+    wl-screenrec --audio --audio-device alsa_output.pci-0000_00_1f.3.hdmi-stereo.monitor -f "$filename" -g "$grid" &
+    # gpu-screen-recorder -w portal -f 60 -a "default_output" -o "$filename" &
+}
+
+stop_recording() {
+    pkill -SIGINT -f "wl-screenrec"
+    notify-send "Screen recording saved to $OUTPUT_DIR"
+}
+
+if pgrep -f "wl-screenrec" > /dev/null; then
+    stop_recording
+else
+    start_recording
+fi
